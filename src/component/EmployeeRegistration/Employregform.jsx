629cf4f8091737fa274ab2c236293101c50e8274
Ranveerjangir <ranveerboy69@gmail.com>
2024-01-04 15:16:08 +0530

    Adding condition that check that no field should be empty during data save

--- src/component/EmployeeRegistration/Employregform.jsx
+++ src/component/EmployeeRegistration/Employregform.jsx
@@ -161,16 +161,6 @@ function FormExample() {
                             </Form.Control.Feedback>
                         </Form.Group>
 
-                        <Form.Group as={Col} md="3" controlId="validationCustom05">
-                            <Form.Label>password</Form.Label>
-                            <Form.Control type="text" placeholder="Enter your Password"  value={password}
-                                onChange={(e) => setPassword(e.target.value)} required />
-                            <Form.Control.Feedback type="invalid">
-                                {Please provide a valid zip.}
-                            {</Form.Control.Feedback> }
-                        {/* </Form.Group> */}
-
-
 
                            <Form.Group as={Col} md="3" controlId="validationCustom05">
                                 <Form.Label>Password</Form.Label>
@@ -185,18 +175,6 @@ function FormExample() {
 
 
                     </Row>
-                    {/* {<Form.Group className="mb-3"> }
-              <Form.Check
-                required
-                label="Agree to terms and conditions"
-                feedback="You must agree before submitting."
-                feedbackType="invalid"
-              />
-            </Form.Group>
-             <Button type="submit">Submit</Button> */}
-
-
-
                      <Form.Group className="mb-3">
                         <Form.Check
                             required
