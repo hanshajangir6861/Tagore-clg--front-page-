981b4e05d871a8eed220d338bb8dce3f38ce0d36
unknown <hanshajangir061@gmail.com>
2024-01-16 11:01:45 -0800

    second change in animations

---
+++ front/src/component/Notice/Notice.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useContext } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import '../Notice/Notice.css'
+import axios from "axios"
+import { context } from '../../App';
+
+
+
+const NoticeForm = () => {
+    const { serverLink } = useContext(context)
+    const [content, setContent] = useState('');
+    const [notificationCount, setNotificationCount] = useState(0);
+
+
+
+    const sendNotice = async () => {
+        let result = await axios.post(`${serverLink}/AdminNotice/notice`, {
+            Content: content,
+        });
+
+        result = result.data;
+    setContent(result);
+    alert('Your Notice has been sent');
+    setContent('');
+    // Increment the notification count
+    setNotificationCount(notificationCount + 1);
+  };
+
+        const student = async () => {
+            let result = await axios.post(`${serverLink}/AdminNotice/notice`, {
+                Content: content
+            })
+            result = result.data
+            setContent(result)
+            alert("Your Notice has been send")
+            setContent('')
+        }
+
+        return (
+            <>
+                <div className='heder'>
+                    <h1>Tagore P.G College Gudha Gorji </h1>
+                    <h2>NOTICE</h2>
+                    <Form>
+                        <Form.Group controlId="formContent">
+                            {/* <Form.Label>Content</Form.Label> */}
+                            <Form.Control
+                                className='textArea'
+                                as="textarea"
+                                // rows={3}
+                                placeholder="Enter notice content"
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                                required
+
+                            />
+                            <div className="btn2">
+                                <Button onClick={(e) => {
+                                    e.preventDefault()
+                                    student()
+                                }}
+                                    type='submit'>Send</Button>
+                            </div>
+                        </Form.Group>
+
+                    </Form>
+                </div>
+            </>
+        );
+    };
+
+    export default NoticeForm;
